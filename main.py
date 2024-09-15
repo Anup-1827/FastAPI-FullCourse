84ca181d18372218316396279f2bfaaa1210a42f
Anup-UsefulBI <anup.usefulbi@gmail.com>
2024-09-15 11:18:51 +0530

    new changes added
    Addded __pycache__  to gitignore

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 from fastapi.params import Body
 from pydantic import BaseModel
+import random
 
 app = FastAPI()
 
@@ -16,23 +17,53 @@ def root2():
 
 # Router '/' --> First
 
-@app.get("/get-post")
+
+post_list = [{
+    "id": "1",
+    "title": "My first post",
+    "content": "This is my first post"
+},
+{
+    "id": "2",
+    "title": "My second post",
+    "content": "This is my second post"
+},
+{
+    "id": "3",
+    "title": "My third post",
+    "content": "This is my third post"
+}
+]
+
+
+def addPost(post):
+    post_list.append(post)
+    return
+
+
+# GET ALL POST
+@app.get("/getAllposts")
 def getpost():
-    return {"message": "Get Post"}
+    return {"post": post_list}
 
 # Post API
-@app.post("/create-post")
-def createPost(payload: dict = Body(...)):
-    print(payload)
-    return {"create-post": payload}
-
-# Schema for Request
 class Post(BaseModel):
-
     title: str
     content: str
+# CREATE POST
+@app.post("/create-post")
+def createPost(payload: Post):   
+    new_post = payload.dict() 
+    new_post['id'] = str(random.randint(0, 1000))
+    addPost(new_post)
+    return {"message": "Post created successfully"}
 
-@app.post("/new-post")
-def newPost(post:Post):
-    print(post)
-    return {"new-post": "created"}
+# Schema for Request
+# class Post(BaseModel):
+#     title: str
+#     content: str
+
+# @app.post("/new-post")
+# def newPost(post:Post):
+#     print(post)
+#     return {"new-post": "created"}  
