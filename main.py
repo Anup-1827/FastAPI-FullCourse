7e98e3125f479e726ddaded9680ff3a2ddde430e
Anup-1827 <anupalone.off@gmail.com>
2024-09-15 20:59:00 +0530

    CRUD Operation Done

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-from fastapi import FastAPI
-from fastapi.params import Body
+from fastapi import FastAPI, HTTPException, status
+from typing import Optional
 from pydantic import BaseModel
 import random
 
@@ -19,17 +19,17 @@ def root2():
 
 
 post_list = [{
-    "id": "1",
+    "id": 1,
     "title": "My first post",
     "content": "This is my first post"
 },
 {
-    "id": "2",
+    "id": 2,
     "title": "My second post",
     "content": "This is my second post"
 },
 {
-    "id": "3",
+    "id": 3,
     "title": "My third post",
     "content": "This is my third post"
 }
@@ -50,20 +50,36 @@ def getpost():
 class Post(BaseModel):
     title: str
     content: str
+    rating: Optional[int] = None
 # CREATE POST
-@app.post("/create-post")
+@app.post("/create-post", status_code=status.HTTP_201_CREATED)
 def createPost(payload: Post):   
     new_post = payload.dict() 
-    new_post['id'] = str(random.randint(0, 1000))
+    new_post['id'] = random.randint(0, 1000)
     addPost(new_post)
     return {"message": "Post created successfully"}
 
-# Schema for Request
-# class Post(BaseModel):
-#     title: str
-#     content: str
 
-# @app.post("/new-post")
-# def newPost(post:Post):
-#     print(post)
-#     return {"new-post": "created"}  
+def find_post_by_id( postList,id: int):
+    for post in postList:
+        if(post["id"] == id):
+            return post
+    return None
+
+@app.get("/post/{id}")
+def getPost(id: int):
+    post =  find_post_by_id(post_list, id)
+    if post:
+        return {"post": post}
+    else:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="No Post Available")
+
+
+@app.delete("/posts/{id}")
+def deletePost(id: int):
+    global post_list
+    print(post_list)    
+    filtered_post_list = [post for post in post_list if post["id"] != id]
+    # print(filtered_post_list)
+    post_list =  filtered_post_list
+    return {"message":"Deleted Successfully!!"}
